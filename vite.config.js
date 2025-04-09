bcede53f0b50cd4eb54245c7b6fdbdf45a64c86e
Michele Nink <mnink@band.com.br>
2025-04-09 18:08:19 -0300

    feat: primeiro commit para o heroku

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), eslintPlugin()],
+    server: {
+        port: 3001,
+    },
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "src"),
+        },
+    },
+});
